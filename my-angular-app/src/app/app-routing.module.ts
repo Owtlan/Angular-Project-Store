6b251fa4719099cff8f1c34b057bbcba43549fc9
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-11-01 15:10:04 +0200

    adding product component to my routes

--- my-angular-app/src/app/app-routing.module.ts
+++ my-angular-app/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
-
+import { ProductAddComponent } from './product-add/product-add.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'add-product', component: ProductAddComponent },
 ];
 
 @NgModule({
