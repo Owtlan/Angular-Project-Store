18d1e682e655b79e459480d611c3d89dc11067ba
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-11-02 14:35:44 +0200

    adding edit/delete in my routes

--- my-angular-app/src/app/app-routing.module.ts
+++ my-angular-app/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component'; // Импортирай новия компонент
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'add-product', component: ProductAddComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+  { path: 'product/edit/:id', component: ProductEditComponent },
+
 ];
 
 @NgModule({
