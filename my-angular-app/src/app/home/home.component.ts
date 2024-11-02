9367b3a7f1c859b1c6412d9ea6acffa00552dfa2
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-11-02 14:36:16 +0200

    update home component ts

--- my-angular-app/src/app/home/home.component.ts
+++ my-angular-app/src/app/home/home.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../model/product.model';
+import { Auth, authState } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+export class HomeComponent implements OnInit {
+  products: Product[] = [];
+  currentUserId: string | null = null;
 
-export class HomeComponent {
+  constructor(private productService: ProductService, private auth: Auth, private router: Router) { }
 
+  ngOnInit(): void {
+    authState(this.auth).subscribe(user => {
+      this.currentUserId = user ? user.uid : null;
+      this.loadProducts();
+    });
+  }
+
+  loadProducts(): void {
+    this.productService.getProducts().subscribe(
+      (data: Product[]) => {
+        this.products = data;
+      },
+      (error) => {
+        console.error('Error loading products:', error);
+      }
+    );
+  }
+
+  goToDetail(productId: string | undefined) {
+    if (productId) {
+      this.router.navigate(['/product', productId]);
+    } else {
+      console.error('Product ID is undefined');
+    }
+  }
 }
+
