286626eb3064f082709837cec59bd74c5eaed588
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-11-02 14:37:22 +0200

    adding cloudinary in my environment

---
+++ my-angular-app/src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyASDj1HNktqIMcqhgbAsMFJ0FZbgVw1Fp4",
+        authDomain: "angular-softuni-project-store.firebaseapp.com",
+        projectId: "angular-softuni-project-store",
+        storageBucket: "angular-softuni-project-store.firebasestorage.app",
+        messagingSenderId: "631156106802",
+        appId: "1:631156106802:web:268eb871881eb24ad53e2b",
+        measurementId: "G-DNMR7CM5C4"
+    },
+    cloudinary: {
+        cloudName: 'dptgsnk7q'
+      }
+};
+
