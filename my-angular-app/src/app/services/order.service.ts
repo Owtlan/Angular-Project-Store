5a91ab49603e1e54f2500578c0acd1b91d4bbcbc
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-11-03 14:13:48 +0200

    adding order.service.ts

---
+++ my-angular-app/src/app/services/order.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Order } from '../model/order.model';
+
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class OrderService {
+
+    constructor(private firestore: Firestore) { } // Или Firestore
+
+    async createOrder(order: Order): Promise<void> {
+        const ordersCollection = collection(this.firestore, 'orders');
+        await addDoc(ordersCollection, order);
+    }
+}
