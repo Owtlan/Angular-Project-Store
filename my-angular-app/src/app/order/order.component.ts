a6708231bcfeb7007a5344d4b9bb4ca01ae1e912
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-11-03 14:13:09 +0200

    order component ts adding placeOrder to make user order correct

---
+++ my-angular-app/src/app/order/order.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { OrderService } from '../services/order.service';
+import { Auth, authState } from '@angular/fire/auth';
+import { Order } from '../model/order.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent {
+  userId: string | null = null;
+
+  shippingDetails = {
+    address: '',
+    city: '',
+    postalCode: '',
+    phoneNumber: '',
+  };
+
+  totalPrice: number = 0;
+
+
+  constructor(
+    private cartService: CartService,
+    private orderService: OrderService,
+    private auth: Auth,
+    private router: Router
+  ) {
+    authState(this.auth).subscribe(user => {
+      this.userId = user ? user.uid : null;
+    })
+  }
+
+
+  placeOrder() {
+    const productsInCart = this.cartService.getCart()
+    this.totalPrice = productsInCart.reduce((total, product) => total + product.price, 0)
+
+
+    const order: Order = {
+      userId: this.userId!,
+      products: productsInCart.map(product => ({ productId: product.id, quantity: 1 })),
+      totalPrice: this.totalPrice,
+      orderDate: new Date(),
+      shippingDetails: this.shippingDetails
+    }
+
+    this.orderService.createOrder(order)
+      .then(() => {
+        console.log('Order placed successfully!');
+      
+        this.cartService.clearCart(); 
+        this.router.navigate(['/'])
+
+      })
+      .catch(error => {
+        console.error('Error placing order:', error);
+      });
+
+  }
+
+}
