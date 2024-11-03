fa660c56167714a1b731dfb1b35c0bbaa9b20765
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-11-03 14:08:52 +0200

    cart component.ts making total price to products + remove product

---
+++ my-angular-app/src/app/cart/cart.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../model/product.model';
+import { CartService } from '../services/cart.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  cartItems: Product[] = [];
+  constructor(private cartService: CartService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.cartItems = this.cartService.getCart(); 
+  }
+
+  getTotalPrice(): number {
+    return this.cartItems.reduce((sum, item) => sum + item.price, 0)
+  }
+  removeFromCart(item: Product): void {
+    this.cartService.removeFromCart(item); 
+    this.cartItems = this.cartService.getCart(); 
+  }
+
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
+
+
+}
