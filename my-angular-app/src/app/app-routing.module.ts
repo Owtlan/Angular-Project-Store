7e211f74daa088956148c509255a07949258adfa
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-11-15 14:44:11 +0200

    adding auth guard + guest guard at my router module

--- my-angular-app/src/app/app-routing.module.ts
+++ my-angular-app/src/app/app-routing.module.ts
@@ -5,22 +5,22 @@ import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component'; // Импортирай новия компонент
+import { ProductEditComponent } from './product-edit/product-edit.component';
 import { CartComponent } from './cart/cart.component';
 import { OrderComponent } from './order/order.component';
 import { ProfileComponent } from './user/profile/profile.component';
-import { AuthGuard } from './auth.guard'
+import { AuthGuard, GuestGuard } from './auth.guard'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
-  { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
-  { path: 'add-product', component: ProductAddComponent },
+  { path: 'login', component: LoginComponent, canActivate: [GuestGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [GuestGuard] },
+  { path: 'add-product', component: ProductAddComponent, canActivate: [AuthGuard] },
   { path: 'product/:id', component: ProductDetailComponent },
-  { path: 'product/edit/:id', component: ProductEditComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'order', component: OrderComponent },
-  { path: 'profile', component: ProfileComponent }
+  { path: 'product/edit/:id', component: ProductEditComponent, canActivate: [AuthGuard] },
+  { path: 'cart', component: CartComponent,canActivate: [AuthGuard] },
+  { path: 'order', component: OrderComponent,canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
