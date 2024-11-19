61159e035dbe58973658fc44592df238a6c2d09e
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-11-19 15:16:42 +0200

    update the way in my routing

--- my-angular-app/src/app/app-routing.module.ts
+++ my-angular-app/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './core/home/home.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
-import { ProductAddComponent } from './product-add/product-add.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductAddComponent } from './product-code/product-add/product-add.component';
+import { ProductDetailComponent } from './product-code/product-detail/product-detail.component';
+import { ProductEditComponent } from './product-code/product-edit/product-edit.component';
 import { CartComponent } from './cart/cart.component';
 import { OrderComponent } from './order/order.component';
 import { ProfileComponent } from './user/profile/profile.component';
