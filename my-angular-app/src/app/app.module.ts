3ab28d845568086776ea08e726af8605bc2b1a90
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-11-19 15:17:56 +0200

    import new paths

--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -7,19 +7,19 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './core/home/home.component';
 import { RegisterComponent } from './user/register/register.component';
 import { LoginComponent } from './user/login/login.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProductAddComponent } from './product-add/product-add.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { ProductAddComponent } from './product-code/product-add/product-add.component';
 
 
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductDetailComponent } from './product-code/product-detail/product-detail.component';
+import { ProductEditComponent } from './product-code/product-edit/product-edit.component';
 import { CartComponent } from './cart/cart.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { OrderComponent } from './order/order.component';
@@ -27,7 +27,7 @@ import { ProfileComponent } from './user/profile/profile.component';
 
 import { register } from 'swiper/element/bundle'
 import { GlobalErrorHandler } from './ErrorHandler/global-error-handler.service';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 register()
 
