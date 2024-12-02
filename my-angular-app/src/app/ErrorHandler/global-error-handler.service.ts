d8ca73d17b9330aa0cdd9ac65fce826b54a7366b
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-12-02 14:32:42 +0200

    after global problem i redirect to home page

--- my-angular-app/src/app/ErrorHandler/global-error-handler.service.ts
+++ my-angular-app/src/app/ErrorHandler/global-error-handler.service.ts
@@ -1,23 +1,25 @@
 // global-error-handler.service.ts
 import { ErrorHandler, Injectable, NgZone } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GlobalErrorHandler implements ErrorHandler {
-  constructor(private ngZone: NgZone) {}
+  constructor(private ngZone: NgZone, private router: Router) { }
 
   handleError(error: any): void {
     this.ngZone.run(() => {
       if (error instanceof HttpErrorResponse) {
-  
+
         console.error('HTTP Error:', error.message);
         alert(`An HTTP error occurred: ${error.status} ${error.statusText}`);
       } else {
- 
+
         console.error('An error occurred:', error);
         alert('An unexpected error occurred. Please try again later.');
+        this.router.navigate(['/'])
       }
     });
   }
